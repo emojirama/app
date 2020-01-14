b97be1799405fec434c1618d97bbe8205a98214e
Brian Caffey <bcaffey@drydengroup.com>
2020-01-13 22:59:46 -0500

    removed old quasar app

--- quasarama/src/boot/components.js
+++
@@ -1,7 +0,0 @@
-// import { Emoji, Picker } from "emoji-mart-vue-fast";
-
-// // leave the export, even if you don't use it
-// export default async ({ Vue }) => {
-//   Vue.component("Emoji", Emoji);
-//   Vue.component("Picker", Picker);
-// };
