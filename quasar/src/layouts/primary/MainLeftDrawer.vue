ca23e0bea37145c9439f078ae9a3bc677f391d1b
Brian Caffey <bcaffey@drydengroup.com>
2020-01-18 10:09:40 -0500

    fixed broken drawer from quasar update

--- quasar/src/layouts/primary/MainLeftDrawer.vue
+++ quasar/src/layouts/primary/MainLeftDrawer.vue
@@ -1,6 +1,7 @@
 <template>
   <q-drawer
     @hide="hideDrawer"
+    @input="hideDrawer"
     @show="showDrawer"
     v-model="leftDrawerOpen"
     content-class="bg-grey-5"
@@ -34,64 +35,6 @@
         icon="info"
         :caption="$t('leftDrawer.about.sub')"
       />
-
-      <!-- <left-menu-link
-        :label="$t('leftDrawer.protected.main')"
-        to="/protected"
-        icon="lock"
-        :caption="$t('leftDrawer.protected.sub')"
-      />
-
-      <left-menu-link
-        :label="$t('leftDrawer.toDo.main')"
-        to="/to-do"
-        icon="check"
-        :caption="$t('leftDrawer.toDo.sub')"
-      />
-
-      <left-menu-link
-        v-if="$store.getters.isAuthenticated"
-        :label="$t('leftDrawer.services.main')"
-        to="/services"
-        icon="insert_chart_outlined"
-        :caption="$t('leftDrawer.services.sub')"
-      />
-      <q-expansion-item
-        :content-inset-level="0.5"
-        v-if="$store.getters.isAuthenticated"
-        expand-separator
-        icon="perm_identity"
-        :label="$t('leftDrawer.examples.main')"
-        :caption="$t('leftDrawer.tests.sub')"
-      >
-        <left-menu-link
-          :label="$t('leftDrawer.examples.websockets.main')"
-          to="/examples/websockets"
-          icon="offline_bolt"
-          :caption="$t('leftDrawer.examples.websockets.sub')"
-        />
-      </q-expansion-item>
-      <q-expansion-item
-        :content-inset-level="0.5"
-        v-if="$store.getters.isAuthenticated"
-        expand-separator
-        icon="check_circle"
-        :label="$t('leftDrawer.tests.main')"
-        :caption="$t('leftDrawer.tests.main')"
-      >
-        <left-menu-link
-          :label="$t('leftDrawer.tests.redis.main')"
-          to="/examples/redis"
-          icon="offline_bolt"
-          :caption="$t('leftDrawer.tests.redis.sub')"
-        />
-      </q-expansion-item>
-      <left-menu-link
-        :label="$t('leftDrawer.environment.main')"
-        to="/debug/environment-variables"
-        icon="offline_bolt"
-        :caption="$t('leftDrawer.environment.main')"
-      /> -->
     </q-list>
   </q-drawer>
 </template>
@@ -116,6 +59,7 @@ export default {
       });
     },
     hideDrawer() {
+      console.log("hiding..");
       this.$store.commit("toggleLeftDrawer", {
         leftDrawerOpen: false
       });
