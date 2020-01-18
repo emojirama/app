905b778fc6a39a6265fb75c1803cc4ccd9c5a2a7
Brian Caffey <bcaffey@drydengroup.com>
2020-01-18 15:35:55 -0500

    removed old url patterns

--- backend/apps/emojirama/urls.py
+++ backend/apps/emojirama/urls.py
@@ -3,26 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path(
-    #     'emojirama/',
-    #     views.list_emojirama,
-    #     name="list_emojirama"
-    # ),
-    # path(
-    #     'emojirama/save/',
-    #     views.save_emojirama,
-    #     name="save_emojirama"
-    # ),
-    # path(
-    #     'emojirama/new/',
-    #     views.new_emojirama,
-    #     name="new_emojirama"
-    # ),
-    # path(
-    #     'emojirama/<int:id>/',
-    #     views.get_emojirama,
-    #     name="get_emojirama"
-    # ),
     path(
         'emojirama/',
         views.EmojiramaViewSet.as_view({
