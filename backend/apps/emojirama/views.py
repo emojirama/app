850cfb4c1491df3785a7a97d376915c119d86a17
Brian Caffey <bcaffey@drydengroup.com>
2020-01-18 15:37:10 -0500

    flake8

--- backend/apps/emojirama/views.py
+++ backend/apps/emojirama/views.py
@@ -1,7 +1,6 @@
-from rest_framework.decorators import api_view
+from rest_framework import viewsets
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.response import Response
-from rest_framework import viewsets
 
 from .models import Emojirama
 from .serializers import EmojiramaSerializer
